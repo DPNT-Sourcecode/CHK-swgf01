ffc062b3ac8865c6af5afa2a1baadc4d407d45d8
sbx_user1060 <sbx_user1060@ip-10-128-250-134.eu-west-2.compute.internal>
2019-04-02 21:59:17 +0000

    Tue Apr 02 21:59:17 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -18,29 +18,29 @@ Notes:
 # noinspection PyUnusedLocal
 # skus = unicode string
 
-from collections import namedtuple
+from collections import namedtuple, defaultdict
 
 SKU = namedtuple('SKU', 'unit_price offer_multiplier offer_price')
 
 price_table = {
-    'A': {
-        SKU(50, 3, 130),
-        'unit_price': 50,
-        'offer_multiplier': 3,
-        'offer_price': 130
-    },
-    'B': {
-        'unit_price': 30,
-        'offer_multiplier': 2,
-        'offer_price': 145
-    },
-    'C': {
-        'price': 20,
-        'offer_multiplier': 1,
-        'offer_price': 145
-    },
+    'A': SKU(50, 3, 130),
+    'B': SKU(30, 2, 45),
+    'C': SKU(20, 1, 20),
+    'D': SKU(15, 1, 15)
 }
 
 
 def checkout(skus):
-    raise NotImplementedError()
+    try:
+        # check input SKUs are present in the price_table
+        # and count occurrences of individual sku
+        sku_cnt = defaultdict(int)
+        for sku in skus:
+            if sku not in price_table:
+                return -1
+            else:
+                sku_cnt[sku] += 1
+    except:
+        # The skus must be iterable for valid input
+        return -1
+
