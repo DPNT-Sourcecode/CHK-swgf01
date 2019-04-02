541eecedb3183d19b297deb54f2a9c1fc65706f7
sbx_user1060 <sbx_user1060@ip-10-128-250-134.eu-west-2.compute.internal>
2019-04-02 21:47:17 +0000

    Tue Apr 02 21:47:17 UTC 2019

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -23,3 +23,20 @@ class TestCHK:
         assert checkout_solution.checkout(1) == -1
         assert checkout_solution.checkout('E') == -1
         assert checkout_solution.checkout('a') == -1
+
+    def test_individual_item(self):
+        assert checkout_solution.checkout('A') == 50
+        assert checkout_solution.checkout('B') == 30
+        assert checkout_solution.checkout('C') == 20
+        assert checkout_solution.checkout('D') == 15
+
+    def test_special_offer_individual(self):
+        assert checkout_solution.checkout('AAA') == 130
+        assert checkout_solution.checkout('AAAA') == 130 + 50
+        assert checkout_solution.checkout('BB') == 45
+        assert checkout_solution.checkout('BBBB') == 90
+        assert checkout_solution.checkout("AAAAA") == 130 + 50*2
+
+
+    def test_combined
+
