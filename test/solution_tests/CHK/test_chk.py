7e9a439fec03d3be2695d11f0560edd66aba3544
sbx_user1060 <sbx_user1060@ip-10-128-59-25.eu-west-2.compute.internal>
2019-04-03 00:20:47 +0000

    Wed Apr 03 00:20:47 UTC 2019

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -5,33 +5,26 @@ price_string = """| A    | 50    | 3A for 130, 5A for 200 |
 | B    | 30    | 2B for 45              |
 | C    | 20    |                        |
 | D    | 15    |                        |
-| E    | 40    | 2E get one B free      |
-| F    | 10    | 2F get one F free      |
-| G    | 20    |                        |
-| H    | 10    | 5H for 45, 10H for 80  |
-| I    | 35    |                        |
-| J    | 60    |                        |
-| K    | 80    | 2K for 150             |
-| L    | 90    |                        |
-| M    | 15    |                        |
-| N    | 40    | 3N get one M free      |
-| O    | 10    |                        |
-| P    | 50    | 5P for 200             |
-| Q    | 30    | 3Q for 80              |
-| R    | 50    | 3R get one Q free      |
-| S    | 30    |                        |
-| T    | 20    |                        |
-| U    | 40    | 3U get one U free      |
-| V    | 50    | 2V for 90, 3V for 130  |
-| W    | 20    |                        |
-| X    | 90    |                        |
-| Y    | 10    |                        |
-| Z    | 50    |                        |"""
+| E    | 40    | 2E get one B free      |"""
 
 
 class TestCHK:
 
-    def test_make_price_table
+    def test_make_price_table(self):
+        price_table = checkout_solution.make_pricetable(price_string)
+        assert price_table == {
+            'A': 50,
+            'B': 30,
+            'C': 20,
+            'D': 15,
+            'E': 40
+        }
+
+    def test_make_special_offers(self):
+        offers = checkout_solution.make_special_offers(price_string)
+        assert checkout_solution.Multiplicative('A', 3, 130) in offers
+        assert checkout_solution.BuyYgetX('E', 2, 'B', 1) in offers
+
 
     def test_invalid_input(self):
         assert checkout_solution.checkout(1) == -1
@@ -64,4 +57,5 @@ class TestCHK:
 
     def test_e_offer(self):
         assert checkout_solution.checkout("ABCDEE") == 50 + 30 + 20 + 15 + 40 * 2 - 30
+
 
